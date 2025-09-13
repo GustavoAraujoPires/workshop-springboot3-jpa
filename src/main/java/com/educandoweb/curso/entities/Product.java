f491e670fce56725fcde0a3ff924cd9ec82e7ecf
Gustavo Araujo Pires <gustavoaraujpires9@gmail.com>
2025-09-12 23:22:23 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/curso/entities/Product.java
+++ src/main/java/com/educandoweb/curso/entities/Product.java
@@ -8,13 +8,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -22,14 +25,14 @@ public class Product implements Serializable {
 	private String description;
 	private Double price;
 	private String imUrl;
-	
-	
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+
 	private Set<Category> categories = new HashSet<>();
-	
-	
+
 	public Product() {
 	}
-
 
 	public Product(Long id, String name, String description, Double price, String imUrl) {
 		super();
@@ -40,61 +43,49 @@ public class Product implements Serializable {
 		this.imUrl = imUrl;
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getDescription() {
 		return description;
 	}
-
 
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-
 	public Double getPrice() {
 		return price;
 	}
-
 
 	public void setPrice(Double price) {
 		this.price = price;
 	}
 
-
 	public String getImUrl() {
 		return imUrl;
 	}
-
 
 	public void setImUrl(String imUrl) {
 		this.imUrl = imUrl;
 	}
 
-
 	public Set<Category> getCategories() {
 		return categories;
 	}
-
 
 	@Override
 	public int hashCode() {
@@ -103,7 +94,6 @@ public class Product implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -121,5 +111,5 @@ public class Product implements Serializable {
 			return false;
 		return true;
 	}
-	
+
 }
