59a624b08c2202eb98968560357774359a1659c5
Gustavo Araujo Pires <gustavoaraujopires9@gmail.com>
2025-11-23 16:18:56 -0300

    Teste

---
+++ src/main/java/com/educandoweb/curso/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.educandoweb.curso.resources.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+
+
+}
